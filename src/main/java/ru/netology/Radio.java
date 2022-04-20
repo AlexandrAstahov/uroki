7dea935ba64b275d801c0612c02a92a0361237ec
AleksandrAstahov <spirtt1985@yandex.ru>
2022-04-20 23:33:44 +0300

    edit Radio.java and RadioTest.java

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,7 +1,7 @@
 package ru.netology;
 
 public class Radio {
-    int numberStation;
+    private int numberStation;
     private int volume;
 
     public int getNumberStation() {
@@ -23,41 +23,79 @@ public class Radio {
     }
 
     public void upStation(int numberStation) {
-        if (numberStation >= 0) {
+        if (numberStation < 0) {
+            return;
+        }
+        if (numberStation == 0) {
             this.numberStation = numberStation + 1;
         }
-        if (numberStation == 10) {
+        if (numberStation < 9) {
+            this.numberStation = numberStation + 1;
+        }
+        if (numberStation == 9) {
+            this.numberStation = 0;
+        }
+        if (numberStation > 9) {
             this.numberStation = 0;
         }
     }
 
     public void downStation(int numberStation) {
-        if (numberStation <= 10) {
+        if (numberStation < 0) {
+            return;
+        }
+        if (numberStation > 9) {
+            return;
+        }
+        if (numberStation < 9) {
             this.numberStation = numberStation - 1;
         }
         if (numberStation == 0) {
-            this.numberStation = 10;
+            this.numberStation = 9;
         }
+        if (numberStation == 9) {
+            this.numberStation = numberStation - 1;
+        }
+
 
     }
 
 
     public void upVolume(int volume) {
-        if (volume >= 0) {
+        if (volume > 10) {
+            return;
+        }
+        if (volume < 0) {
+            return;
+        }
+        if (volume == 10) {
+            this.volume = 10;
+        }
+        if (volume == 0) {
             this.volume = volume + 1;
         }
-        if (volume >= 10) {
-            this.volume = 10;
+        if (volume < 10) {
+            this.volume = volume + 1;
         }
     }
 
     public void downVolume(int volume) {
-        if (volume <= 10) {
+        if (volume > 10) {
+            return;
+        }
+        if (volume < 0) {
+            return;
+        }
+        if (volume == 0) {
+            return;
+        }
+        if (volume == 10) {
             this.volume = volume - 1;
         }
-        if (volume <= 0) {
-            this.volume = 0;
+        if (volume < 10) {
+            this.volume = volume - 1;
         }
+
     }
 }
 
--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -9,22 +9,43 @@ class RadioTest {
     @Test
     public void shouldUpVolume(){
         Radio radio = new Radio();
-        radio.upVolume(11);
-        assertEquals(10, radio.getVolume());
+        radio.upVolume(6);
+        assertEquals(7, radio.getVolume());
+    }
+
+    @Test
+    public void shouldUpVolumeBelowRange(){
+        Radio radio = new Radio();
+        radio.upVolume(-1);
+        assertEquals(0, radio.getVolume());
     }
 
     @Test
     public void shouldUpVolumeAboveRange(){
         Radio radio = new Radio();
         radio.upVolume(11);
+        assertEquals(0, radio.getVolume());
+    }
+
+    @Test
+    public void shouldUpVolumeMax(){
+        Radio radio = new Radio();
+        radio.upVolume(10);
         assertEquals(10, radio.getVolume());
+    }
+
+    @Test
+    public void shouldUpVolumeMin(){
+        Radio radio = new Radio();
+        radio.upVolume(0);
+        assertEquals(1, radio.getVolume());
     }
 
     @Test
     public void shouldDownVolume(){
         Radio radio = new Radio();
-        radio.downVolume(2);
-        assertEquals(1, radio.getVolume());
+        radio.downVolume(8);
+        assertEquals(7, radio.getVolume());
     }
 
     @Test
@@ -35,14 +56,77 @@ class RadioTest {
     }
 
     @Test
+    public void shouldDownVolumeAboveRange(){
+        Radio radio = new Radio();
+        radio.downVolume(11);
+        assertEquals(0, radio.getVolume());
+    }
+
+    @Test
+    public void shouldDownVolumeMax(){
+        Radio radio = new Radio();
+        radio.downVolume(10);
+        assertEquals(9, radio.getVolume());
+    }
+
+    @Test
+    public void shouldDownVolumeMin(){
+        Radio radio = new Radio();
+        radio.downVolume(0);
+        assertEquals(0, radio.getVolume());
+    }
+
+    @Test
+    public void shouldSetNumberStation (){
+        Radio radio = new Radio();
+        radio.setNumberStation(4);
+        assertEquals(4,radio.getNumberStation());
+    }
+
+    @Test
+    public void shouldSetNumberStationBelowRange (){
+        Radio radio = new Radio();
+        radio.setNumberStation(-1);
+        assertEquals(0,radio.getNumberStation());
+    }
+
+    @Test
+    public void shouldSetNumberStationAboveRange (){
+        Radio radio = new Radio();
+        radio.setNumberStation(10);
+        assertEquals(0,radio.getNumberStation());
+    }
+
+    @Test
     public void shouldUpStation (){
+        Radio radio = new Radio();
+        radio.upStation(7);
+        assertEquals(8,radio.getNumberStation());
+    }
+
+    @Test
+    public void shouldUpStationMax (){
+        Radio radio = new Radio();
+        radio.upStation(9);
+        assertEquals(0,radio.getNumberStation());
+    }
+
+    @Test
+    public void shouldUpStationMin (){
         Radio radio = new Radio();
         radio.upStation(0);
         assertEquals(1,radio.getNumberStation());
     }
 
     @Test
-    public void shouldUpStationMax (){
+    public void shouldUpStationBelowRange (){
+        Radio radio = new Radio();
+        radio.upStation(-1);
+        assertEquals(0,radio.getNumberStation());
+    }
+
+    @Test
+    public void shouldUpStationAboveRange (){
         Radio radio = new Radio();
         radio.upStation(10);
         assertEquals(0,radio.getNumberStation());
@@ -51,36 +135,35 @@ class RadioTest {
     @Test
     public void shouldDownStation (){
         Radio radio = new Radio();
-        radio.downStation(8);
-        assertEquals(7,radio.getNumberStation());
+        radio.downStation(7);
+        assertEquals(6,radio.getNumberStation());
+    }
+
+    @Test
+    public void shouldDownStationMax (){
+        Radio radio = new Radio();
+        radio.downStation(9);
+        assertEquals(8,radio.getNumberStation());
     }
 
     @Test
     public void shouldDownStationMin (){
         Radio radio = new Radio();
         radio.downStation(0);
-        assertEquals(10,radio.getNumberStation());
+        assertEquals(9,radio.getNumberStation());
     }
 
     @Test
-    public void shouldsetNumberStation (){
+    public void shouldDownStationBelowRange (){
         Radio radio = new Radio();
-        radio.setNumberStation(4);
-        assertEquals(4,radio.getNumberStation());
+        radio.downStation(-1);
+        assertEquals(0,radio.getNumberStation());
     }
 
     @Test
-    public void shouldsetNumberStationAboveTheLimit () {
+    public void shouldDownStationAboveRange (){
         Radio radio = new Radio();
-        radio.setNumberStation(10);
-        assertEquals(0, radio.getNumberStation());
+        radio.downStation(10);
+        assertEquals(0,radio.getNumberStation());
     }
-
-    @Test
-    public void shouldsetNumberStationBelowLimit () {
-        Radio radio = new Radio();
-        radio.setNumberStation(-1);
-        assertEquals(0, radio.getNumberStation());
-    }
-
 }
